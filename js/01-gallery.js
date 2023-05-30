d327c960cce3862f9a21194009eb3b7ed5e80699
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-05-30 14:34:47 +0300

    g

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+// create li elements images
+createImageItems();
+
+function createImageItems() {
+  const importImageOnScreen = createImageElements(galleryItems);
+  galleryList.innerHTML = importImageOnScreen.join('');
+}
+
+function createImageElements(items) {
+  return items.map(item => `<li class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" /></a></li>`);
+}
+
+// create modal click
+const onContainerClick = (el) => {
+  el.preventDefault();
+
+  if (el.target.classList.contains('gallery')) return;
+  const source = el.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+
+  instance.show();
+};
+
+galleryList.addEventListener('click', onContainerClick);
+
+document.addEventListener('keydown', function(el) {
+  if (el.key === 'Escape') {
+    hideModal();
+  }
+});
+
+function hideModal() {
+  const modal = document.querySelector('.basicLightbox');
+  modal.classList.remove('basicLightbox--visible');
+}
