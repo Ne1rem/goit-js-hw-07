49186e503c8d09973e96c9876a855a6fae0c9332
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-05-31 16:31:42 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,11 +11,12 @@ function createImageItems() {
 }
 
 function createImageElements(items) {
-  return items.map(item => `<li class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" /></a></li>`);
+  return items.map(item => `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" /></a></li>`);
 }
 
-const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "alt",
-    captionDelay: 250,
+const lightbox = new SimpleLightbox(`.gallery a`, { 
+    captionsData: "alt",  
     captionType: "alt",
+    captionDelay: 250,
+  
   });
